ea3fff6842eb0eb42a7b04ad56a4ad18f0f0484a
Blago <blago.culjak@hotmail.com>
2023-07-17 14:24:41 +0200

    Init code

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Counter from './components/Counter.vue';
+import Counter2 from './components/Counter2.vue';
+</script>
+
+<template>
+  <div class="flex flex-row justify-around w-full">
+    <div class=" bg-gray-900 h-screen w-full justify-evenly">
+      <img src="./assets/logo.svg" width="200" class="m-auto mt-32"/>
+      <img src="./assets/logo.svg" width="200" class="m-auto mt-32"/>
+    </div>
+    
+    <div class=" w-full">
+    <Counter />
+  <Counter2 />
+</div>
+<div class=" bg-gray-900 h-screen w-full justify-evenly">
+      <img src="./assets/logo.svg" width="200" class="m-auto mt-32"/>
+      <img src="./assets/logo.svg" width="200" class="m-auto mt-32"/>
+    </div>
+  </div>
+
+</template>
---
+++ src/components/Counter2.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import PrimaryButton from './PrimaryButton.vue';
+import DangerButton from './DangerButton.vue';
+
+const count = ref(0);
+</script>
+
+<template>
+    <p class="w-full mt-8 flex justify-center font-orbitron">Limited edition</p>
+  <p class="w-full flex justify-center font-orbitron text-2xl">BEEFSTEAK WRAP:</p>
+  <div class="flex justify-center">
+    <span ref="counter" class="text-5xl font-bold text-red-600 font-orbitron">{{ count }} / 30</span>
+  </div>
+  <div class="w-full mt-4 flex flex-row space-x-4 justify-center">
+    <primary-button ref="decrementButton" title="-" @click="count--" />
+    <primary-button ref="incrementButton" title="+" @click="count++" class=" bg-green-600 text-xl" />
+  </div>
+  <div class="w-full mt-4 flex flex-row space-x-4 justify-center">
+    <danger-button ref="resetButton" title="Reset" @click="count = 0" />
+  </div>
+</template>
